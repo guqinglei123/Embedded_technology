314d0b94da0baebaa78bf21cf276fb27bfca3831
guqinglei123 <1092734906@qq.com>
2018-07-26 15:09:10 +0800

    0627

--- TQ2440/Part_4/chapter_9/led.c
+++ TQ2440/Part_4/chapter_9/led.c
@@ -11,8 +11,8 @@
 unsigned int *led_config; 
 unsigned int *led_data; 
 
-struct cdev cdev;
-dev_t devno;
+struct cdev cdev;//描述字符设备的结构体
+dev_t devno;//设备号
 
 int led_open(struct inode *node, struct file *filp)
 {
@@ -40,23 +40,24 @@ long led_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	    	return -EINVAL;
 	}
 }
-
+//与虚拟文件系统的接口
 static struct file_operations led_fops =
 {
     .open = led_open,
     .unlocked_ioctl = led_ioctl,
 };
 
+//加载函数
 static int led_init()
 {
-    cdev_init(&cdev,&led_fops);
+    cdev_init(&cdev,&led_fops);//将字符设备的结构体与file_operations
     
     alloc_chrdev_region(&devno, 0 , 1 , "myled");
-    cdev_add(&cdev, devno, 1);
+    cdev_add(&cdev, devno, 1);//获得设备号
     
     return 0;	
 }
-
+//退出函数
 static void led_exit()
 {
 	cdev_del(&cdev);
