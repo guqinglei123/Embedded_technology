31e0c9deba41fedb209f1d4a7d1c25117631bd87
guqinglei123 <1092734906@qq.com>
2019-01-27 18:51:24 +0800

    修改

--- Programming_language/linux_c/hello_world.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("hello,world.\n");
-	printf("character:%c\ninteger:%d\nfloating point:%f\n",'{',34,3.14);
-	return 0;
-}
--- temporary/Test/Standard_Example/src/sub.cpp
+++
@@ -1,3 +0,0 @@
-#include "../include/sub.h"
-
-int sub(int a, int b) { return a - b; }
